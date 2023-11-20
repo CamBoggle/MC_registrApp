3a6c3ea3e8af069a95c15892a87106300e1ee311
VvN123 <heero7_@hotmail.com>
2023-11-20 03:26:55 -0300

    1

--- src/app/inicio/api-login.service.ts
+++ src/app/inicio/api-login.service.ts
@@ -56,33 +56,51 @@ export class ApiLoginService {
     return idAsistencia;
   }
 
-async alumnoPresente(idAsistencia: string, idUsuario: string) {
-  const asistenciaRef = ref(this.db, `Asistencia/${idAsistencia}/alumno_presente`);
+// async alumnoPresente(idAsistencia: string, idUsuario: string) {
+//   const asistenciaRef = ref(this.db, `Asistencia/${idAsistencia}/alumno_presente`);
 
-  // Obtener el objeto actual de 'alumno_presente'
-  const snapshot = await get(asistenciaRef);
+//   // Obtener el objeto actual de 'alumno_presente'
+//   const snapshot = await get(asistenciaRef);
 
-  // Definir el tipo de 'alumnosPresentes' como un objeto cuyas claves son strings
-  let alumnosPresentes: { [key: string]: string } = {};
+//   // Definir el tipo de 'alumnosPresentes' como un objeto cuyas claves son strings
+//   let alumnosPresentes: { [key: string]: string } = {};
 
-  if (snapshot.exists() && typeof snapshot.val() === 'object') {
-    // Si el objeto ya existe, lo copiamos
-    alumnosPresentes = snapshot.val();
+//   if (snapshot.exists() && typeof snapshot.val() === 'object') {
+//     // Si el objeto ya existe, lo copiamos
+//     alumnosPresentes = snapshot.val();
+//   }
+
+//   // Generar una clave única para el nuevo usuario
+//   const newKeyRef = push(asistenciaRef);
+//   const newKey = newKeyRef.key;
+
+//   // Asegurarse de que newKey no sea null
+//   if (newKey !== null && !Object.values(alumnosPresentes).includes(idUsuario)) {
+//     alumnosPresentes[newKey] = idUsuario;
+//     // Actualizar el objeto en Firebase
+//     await update(asistenciaRef, alumnosPresentes);
+//   }
+// }
+
+async alumnoPresente(idAsistencia: string, idUsuario: string): Promise<void> {
+  const attendanceRef = ref(this.db, `Asistencia/${idAsistencia}`);
+  try {
+    const snapshot = await get(attendanceRef);
+    if (snapshot.exists()) {
+      const attendance = snapshot.val();
+      attendance.attendance_students = attendance.attendance_students || [];
+      if (!attendance.attendance_students.includes(idUsuario)) {
+        attendance.attendance_students.push(idUsuario);
+        await set(attendanceRef, attendance);
+      }
+    } else {
+      console.log('Attendance record not found');
+    }
+  } catch (error) {
+    console.error('Error updating attendance record:', error);
+    throw error;
   }
-
-  // Generar una clave única para el nuevo usuario
-  const newKeyRef = push(asistenciaRef);
-  const newKey = newKeyRef.key;
-
-  // Asegurarse de que newKey no sea null
-  if (newKey !== null && !Object.values(alumnosPresentes).includes(idUsuario)) {
-    alumnosPresentes[newKey] = idUsuario;
-    // Actualizar el objeto en Firebase
-    await update(asistenciaRef, alumnosPresentes);
-  }
-}
-
-  
+} 
   
 
 }
