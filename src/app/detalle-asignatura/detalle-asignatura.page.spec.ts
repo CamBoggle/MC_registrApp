d31b8243df54ad14457f0722dde15369097f9026
CamBoggle <camilo.boggle@gmail.com>
2023-11-22 10:16:55 -0300

    actualizacion

--- src/app/detalle-asignatura/detalle-asignatura.page.spec.ts
+++ src/app/detalle-asignatura/detalle-asignatura.page.spec.ts
@@ -1,17 +1,50 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed,  fakeAsync, waitForAsync  } from '@angular/core/testing';
 import { DetalleAsignaturaPage } from './detalle-asignatura.page';
+//Import adicionales
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
 
-describe('DetalleAsignaturaPage', () => {
+// describe('DetalleAsignaturaPage', () => {
+//   let component: DetalleAsignaturaPage;
+//   let fixture: ComponentFixture<DetalleAsignaturaPage>;
+
+//   beforeEach(async(() => {
+//     fixture = TestBed.createComponent(DetalleAsignaturaPage);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   }));
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
+
+describe('==== Prueba detalle Asignatura ====', () => {
   let component: DetalleAsignaturaPage;
   let fixture: ComponentFixture<DetalleAsignaturaPage>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync( async() => {
+    await TestBed.configureTestingModule({
+      declarations: [DetalleAsignaturaPage],
+      imports : [IonicModule.forRoot(), FormsModule, RouterTestingModule]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(DetalleAsignaturaPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+
+  it('Alumnos Presentes: El metodo contara los alumbnos presentes en clases',() => {
+    const mockAsistencia = { alumno_presente: ['alumno1', 'alumno2', 'alumno3'] };
+    const resultado = component.contarAlumnosPresentes(mockAsistencia);
+    expect(resultado).toEqual(3);
+  });
+
+  it('Alumnos Presentes: No hay alumnos en la clase',() => {
+    const mockAsistencia = { alumno_presente: [] };
+    const resultado = component.contarAlumnosPresentes(mockAsistencia);
+    expect(resultado).toEqual(0);
   });
 });
--- src/app/dos.profesor/dos.profesor.page.spec.ts
+++ src/app/dos.profesor/dos.profesor.page.spec.ts
@@ -1,11 +1,16 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync,fakeAsync } from '@angular/core/testing';
 import { DosProfesorPage } from './dos.profesor.page';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
+
 
 describe('DosProfesorPage', () => {
   let component: DosProfesorPage;
   let fixture: ComponentFixture<DosProfesorPage>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     fixture = TestBed.createComponent(DosProfesorPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
